5e97542b836ff14283c0c08b7896786ec91624d9
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-09-11 04:03:39 -0400

    first commit

---
+++ example_client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    // "github.com/golang/protobuf/proto"
+    pb "./service_updater_protocol"
+)
+
+func main() {
+    conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+    if err != nil {
+        log.Fatalf("did not connect: %v", err)
+    }
+    defer conn.Close()
+    c := pb.NewServiceUpdaterClient(conn)
+
+    r, err := c.UpdateService(context.Background(), &pb.UpdateRequest{ServiceName: "polymer-demo", ImageName: "cookinrelaxin/polymer-demo:18"})
+    if err != nil {
+        log.Fatalf("could not update service: %v", err)
+    }
+    // log.Printf("Response: %s", r.Message)
+    log.Printf("%#v", r)
+}
+
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "log"
+    "net"
+    "os/exec"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    pb "./service_updater_protocol"
+)
+
+type server struct {}
+
+func (s *server) UpdateService(ctx context.Context, in *pb.UpdateRequest) (*pb.UpdateResponse, error) {
+    serviceName := in.ServiceName
+    imageName := in.ImageName
+    log.Printf("Attemping to update service %s with image %s", serviceName, imageName)
+
+    updateCmd := exec.Command("docker", "service", "update", "--image", imageName, serviceName)
+    out, err := updateCmd.CombinedOutput()
+    if err != nil {
+        log.Printf("Failed to update: %v", string(out))
+        return &pb.UpdateResponse{Status: pb.UpdateResponse_FAILURE, Message: string(out)}, nil
+    }
+    log.Printf("Successfully updated service")
+
+    return &pb.UpdateResponse{Status: pb.UpdateResponse_SUCCESS, Message: string(out)}, nil
+}
+
+func main() {
+    lis, err := net.Listen("tcp", "localhost:8080")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    grpcServer := grpc.NewServer()
+    pb.RegisterServiceUpdaterServer(grpcServer, &server{})
+    grpcServer.Serve(lis)
+}
